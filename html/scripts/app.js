c26452d42a36144ec188dabb434004514d1f56da
@andreyhtml1997 <andreyhtml1997@gmail.com>
2021-12-21 19:30:36 +0200

    blog1.html, components.html

--- html/scripts/app.js
+++ html/scripts/app.js
@@ -9,3 +9,34 @@ window.onload = function () {
 $('.menuMobileToggle--js').on('click', function() {
   $('.menuMobile').toggleClass('active');
 });
+
+if($('.carousel-3els').length) {
+  $('.carousel-3els').owlCarousel({
+    dots: true,
+    
+    navText: ['<i class="fas fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>'],
+    responsive: {
+      0: {
+        items: 1
+      },
+      575: {
+        items: 2,
+        margin: 20
+      },
+      767: {
+        items: 2,
+        nav:false,
+        margin: 20
+      },
+      991: {
+        items: 3,
+        nav:true,
+        margin: 20
+      },
+      1199: {
+        nav:true,
+        margin: 40
+      },
+    }
+  });
+}
