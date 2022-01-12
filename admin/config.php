19d1b1a13206d803bf34bbde3e143ab4fbcfdb19
San4ez-mc <olexandrmatsuk@gmail.com>
2022-01-12 16:11:17 +0200

    open server

---
+++ admin/config.php
@@ -0,0 +1,37 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://makeme.local/admin/');
+define('HTTP_CATALOG', 'http://makeme.local/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://makeme.local/admin/');
+define('HTTPS_CATALOG', 'http://makeme.local/');
+
+// DIR
+define('DIR_APPLICATION', 'E:/RunninPorgs/OS21/OSPanel/domains/makeme.local/admin/');
+define('DIR_SYSTEM', 'E:/RunninPorgs/OS21/OSPanel/domains/makeme.local/system/');
+define('DIR_IMAGE', 'E:/RunninPorgs/OS21/OSPanel/domains/makeme.local/image/');
+define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
+define('DIR_CATALOG', 'E:/RunninPorgs/OS21/OSPanel/domains/makeme.local/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'makeme');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
+define('OPENCARTFORUM_SERVER', 'https://opencartforum.com/');
